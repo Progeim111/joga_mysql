8c7ec51c628239032083b0599079154847b7209f
Progeim111 <102576858+Progeim111@users.noreply.github.com>
2023-02-01 13:42:58 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
 const express = require('express')
 const app = express()
+
 const path = require('path')
-const mysql = require('mysql')
 
 const hbs = require('express-handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: __dirname + '/views/layouts/'
+    layoutsDir:__dirname+ '/views/layouts/',
 }))
 
 app.use(express.static('public'));
 
+const mysql = require('mysql')
 const bodyParser = require('body-parser')
+
 app.use(bodyParser.urlencoded({extended:true}))
 
-var con = mysql.createConnection({
-    host: 'Localhost',
-    user: 'root',
-    password: 'qwerty',
-    database: 'joga_mysql'
+let con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "qwerty",
+    database: "joga_mysql",
 })
 
-con.connect(function (err) {
+con.connect(function (err){
     if (err) throw err;
-    console.log('Connected to joga_mysql db!');
-})
+    console.log('connected to joga_mysql db')
 
-app.listen(3030, () => {
-    console.log('App is started at http://localhost:3030');
+})
+app.get("/", (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result)=>{
+        if (err) throw err
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
 });
+app.get('/article/:slug', (req, res) => {
+    let query = `SELECT * FROM article where slug="${req.params.slug}"`;
+    let article
+    con.query(query, (err, result)=>{
+        if (err) throw err
+        article = result
+        res.render('article', {
+            article: article
+        })
+    })
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception: ', err);
+});
+
+app.listen(3000, () =>{
+    console.log('app us started at http://localhost:3000');
+})
